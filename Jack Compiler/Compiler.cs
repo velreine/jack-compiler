07a2a026ce56a01cb740bed9fb71238b29c64682
Nicky Hansen <nicky-g-hansen@hotmail.com>
2022-02-08 08:57:29 +0100

    initial commit.

---
+++ Jack Compiler/Compiler.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Jack_Compiler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Benjamin is shit");
+        }
+    }
+}
